4c8770c097ebb93b956d0daa1d35a9b1b8556f40
Reinaldo Matheus <reinalddo@gmail.com>
2026-03-21 09:20:26 -0400

    Limpiar caché y aplicar reglas de gitignore

--- includes/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Conexión a la base de datos para todos los módulos
-//$mysqli = new mysqli('localhost', 'u680460687_vgaming', 'LnGxQW:b0Y', 'u680460687_vgaming');
-$mysqli = new mysqli('localhost', 'root', '', 'virtualgaming');
-if ($mysqli->connect_errno) {
-    die('Error de conexión: ' . $mysqli->connect_error);
-}
-$mysqli->set_charset('utf8mb4');
